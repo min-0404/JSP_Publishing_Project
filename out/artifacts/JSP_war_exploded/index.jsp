54ccaf32e78e5c4921872b4b99bb335e95a19410
minseok <minsuck040484@gmail.com>
2023-04-16 21:03:02 +0900

    New Jsp Project Created(20230416)

---
+++ out/artifacts/JSP_war_exploded/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 김민석
+  Date: 2023-04-16
+  Time: 오후 7:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  <a href="./Hello.test" methods="get">Servlet으로 넘어갑니다.</a>
+  </body>
+</html>
---
+++ src/Servlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "*.test")
+public class Servlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+        out.print("<p> Hello, Servlet!</p>");
+    }
+}
